d08b354c9872db3981964aeff91060c3c7c302c0
Antonio Crispin <antonio.crispin@gmail.com>
2019-12-08 16:47:42 -0500

    fix import

--- src/main/java/com/unicon/api/commons/db/ServiceCommonsDbApplication.java
+++ src/main/java/com/unicon/api/commons/db/ServiceCommonsDbApplication.java
@@ -1,6 +1,5 @@
 package com.unicon.api.commons.db;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
